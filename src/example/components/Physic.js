de0514b1de5d04cc35e20d9670ecfdc088cfff41
pinn3 <iam@lacking.education>
2017-07-28 22:07:28 +0200

    Replace Physic component default with getDefaults to prevent shared properties

--- src/example/components/Physic.js
+++ src/example/components/Physic.js
@@ -2,11 +2,11 @@
 
 const Physic = {
   name: 'physic',
-  defaults: {
+  getDefaults: () => ({
     vel: { x: 0, y: 0 },
     cor: { x: 0, y: 0 },
     onGround: false,
-  }
+  })
 }
 
 export default Physic
